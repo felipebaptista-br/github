de70ff600ace07bebec4774052291617342e964d
Felipe Baptista <fbaptista@lyra.com>
2025-05-20 20:11:08 -0300

    fix: Error (Type '{ children: ReactNode; searchParams: ExploreProps; }' is not valid.)

--- src/app/(public)/explore/(app)/layout.tsx
+++ src/app/(public)/explore/(app)/layout.tsx
@@ -1,10 +1,7 @@
 import React, { ReactNode } from "react"
 
-type ExploreProps = Promise<{ sidebar: string }>
-
 export default async function LayoutExplore(props: {
   children: ReactNode
-  searchParams: ExploreProps
 }) {
   const { children } = props
 
